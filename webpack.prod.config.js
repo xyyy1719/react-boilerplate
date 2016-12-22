495b8b08c137f1850ce94d5ece0ca7e1716094ef
xyyy1719 <xyyy1719@outlook.com>
2016-12-22 20:38:19 +0800

    config babel/webpack/eslint

---
+++ webpack.prod.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: "bundle.js"
+    },
+    resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                include: PATHS.app,
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                include: PATHS.app,
+                loader: 'style!css?modules'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+}
